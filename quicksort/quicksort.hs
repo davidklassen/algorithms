a0b788deb33e2bc19c2abe83f4e81b144474f161
David Klassen <f0rk.tt@gmail.com>
2013-10-28 00:23:28 +0700

    added haskell version of quicksort

---
+++ quicksort/quicksort.hs
@@ -0,0 +1,13 @@
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort left ++ [x] ++ qsort right
+               where left = [a | a <- xs, a <= x]
+                     right = [a | a <- xs, a > x]
+
+-- input handling
+
+main = do 
+  content <- getContents
+  let arr = map read $ lines content :: [Int]
+  mapM_ print $ qsort arr
+
