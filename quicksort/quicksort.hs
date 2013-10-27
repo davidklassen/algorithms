9594207e435776bf1c3c6fe799cb6bb3a5346a79
David Klassen <f0rk.tt@gmail.com>
2013-10-28 00:25:41 +0700

    fixed codestyle

--- quicksort/quicksort.hs
+++ quicksort/quicksort.hs
@@ -1,12 +1,12 @@
 qsort :: Ord a => [a] -> [a]
 qsort [] = []
 qsort (x:xs) = qsort left ++ [x] ++ qsort right
-               where left = [a | a <- xs, a <= x]
-                     right = [a | a <- xs, a > x]
+               where left = [ a | a <- xs, a <= x ]
+                     right = [ a | a <- xs, a > x ]
 
 -- input handling
 
-main = do 
+main = do
   content <- getContents
   let arr = map read $ lines content :: [Int]
   mapM_ print $ qsort arr
